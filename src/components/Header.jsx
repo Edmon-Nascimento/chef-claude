2504fdaca9c58149e2f3f66f0fe7fd31ee9a64b2
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-05-29 20:25:48 -0300

    feat: create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="w-full flex items-center justify-center py-8 border-b border-black/10">
+      <img src="" alt="" />
+      <h1 className="text-4xl">Chef Claude</h1>
+    </header>
+  );
+}
